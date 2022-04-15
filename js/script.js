bdd7e63a10eafa152b7274112a581e50d4719629
Andrey35211 <91612901+Andrey35211@users.noreply.github.com>
2022-04-15 17:17:50 +0700

    upload files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const navLinks = document.querySelectorAll('.nav-item');
+const menuToggle = document.getElementById('navbarNav');
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { menuToggle.classList.toggle('show') })
+});
+
